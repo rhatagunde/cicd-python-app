d87a86e87156fca1fc78f602f5d95735e37de305
rhatagunde <80765104+rhatagunde@users.noreply.github.com>
2026-04-20 16:19:56 +0530

    Update Docker run command to use specific tagname

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -5,4 +5,4 @@ set -e
 docker push rhatagunde/simple-python-flask-app:tagname
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 rhatagunde/simple-python-flask-app
+docker run -d -p 5000:5000 rhatagunde/simple-python-flask-app:tagname
