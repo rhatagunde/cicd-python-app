0a8d223a55f600e9fdc2a252b39e1529e5d62153
rhatagunde <80765104+rhatagunde@users.noreply.github.com>
2026-04-20 18:01:29 +0530

    Fix variable name for container ID in stop script

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -2,6 +2,6 @@
 set -e
 
 # Stop the running container (if any)
-container_id: 'docker ps | awk -F " " '{print $1}''
-docker rm -f $container_id
+ContainerID: 'docker ps | awk -F " " '{print $1}''
+docker rm -f $ContainerID
 #echo "hi"
