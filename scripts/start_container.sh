fe89ddbf579faccb56c6acb4eee63964a8f0b7d9
rhatagunde <80765104+rhatagunde@users.noreply.github.com>
2026-04-20 16:23:28 +0530

    Change script to pull Docker image instead of push

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker push rhatagunde/simple-python-flask-app:tagname
+docker pull rhatagunde/simple-python-flask-app
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 rhatagunde/simple-python-flask-app:tagname
+docker run -d -p 5000:5000 rhatagunde/simple-python-flask-app
